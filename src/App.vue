b70a0d50d4057ab7058db09b6d4d385ea137fd52
Karim90403 <karim90403@gmail.com>
2023-04-25 15:27:03 +0300

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
 interface Deal{
   name: String,
@@ -11,26 +11,50 @@ interface Deal{
 }
 
 const Deals = ref<Array<Deal>>([
-  { name: "Сделка 1", status: "Первичный контакт", ResponsiblePerson: "Первичный контакт", date: "20 января 2023" , Budget: 1000 },
-  { name: "Сделка 2", status: "Первичный контакт", ResponsiblePerson: "Первичный контакт", date: "20 января 2023" , Budget: 1000 },
-  { name: "Сделка 3", status: "Первичный контакт", ResponsiblePerson: "Первичный контакт", date: "20 января 2023" , Budget: 1000 },
-  { name: "Сделка 4", status: "Первичный контакт", ResponsiblePerson: "Первичный контакт", date: "20 января 2023" , Budget: 1000 },
+  { name: "--------", status: "-----------------", ResponsiblePerson: "-------------", date: "-----" , Budget: 0 },
+  { name: "--------", status: "-----------------", ResponsiblePerson: "-------------", date: "-----" , Budget: 0 },
+  { name: "--------", status: "-----------------", ResponsiblePerson: "-------------", date: "-----" , Budget: 0 }
 ])
+
+let textField: string
+
+const getData = async () => {
+  if(textField.length >= 3){
+    try {
+      let res = await axios.get("http://localhost:8014/api/getData", {params: {filterName : textField}})
+      Deals.value = res.data
+    } catch(err){
+      console.log(err);
+    }
+  }
+}
+
+onMounted(async () => {
+  try {
+    let res = await axios.get("http://localhost:8014/api/getData")
+    Deals.value = res.data
+  } catch(err){
+    console.log(err);
+  }
+})
 </script>
 
 <template>
 <div class="d-flex justify-space-around pt-16">
   <span class="text-h4">Сделки компании "Тестоваое задание"</span>
-  <v-responsive
-    max-width="344"
-  >
-  <v-text-field
-    append-icon="mdi-magnify"
-    label="Search"
-    single-line
-    hide-details
-  ></v-text-field>
-  </v-responsive>
+  <div class="d-flex">
+    <v-responsive
+      width="344"
+    >
+      <v-text-field
+        label="Название сделки"
+        single-line
+        hide-details
+        v-model="textField"
+      ></v-text-field>
+    </v-responsive>
+    <v-btn icon="mdi-magnify" variant="text" class="mx-2" @click="getData"></v-btn>
+  </div>
 </div>
 
 <v-table height="60vh" fixed-header class="pa-8">
@@ -51,20 +75,17 @@ const Deals = ref<Array<Deal>>([
         <th class="text-left">
           Бюджет
         </th>
-        <th class="text-left"></th>
       </tr>
     </thead>
     <tbody>
       <tr
         v-for="deal in Deals"
-        :key="deal.name"
       >
         <td>{{ deal.name }}</td>
         <td>{{ deal.status }}</td>
         <td>{{ deal.ResponsiblePerson }}</td>
         <td>{{ deal.date }}</td>
         <td>{{ deal.Budget }}₽</td>
-        <td><v-btn density="compact" icon="mdi-plus"></v-btn></td>
       </tr>
     </tbody>
   </v-table>
